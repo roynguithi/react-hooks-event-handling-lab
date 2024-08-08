eb9366e4861b5cfe028e335f71ad0e408a73e999
roy collins nguithi <roynguithi@student.moringaschool.com>
2024-08-08 09:06:13 +0300

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,22 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
+// Define the Keypad component as a class component
+class Keypad extends React.Component {
+
+  // Event handler for the change event
+  handleChange = () => {
+    console.log('Entering password...'); // Log a message when the input value changes
+  };
+
+  // Render method to output the component's UI
+  render() {
     return (
-        <div></div>
-    )
+      <input
+        type="password"       // Define the input type as password
+        onChange={this.handleChange} // Attach the change event handler
+      />
+    );
+  }
 }
 
-export default Keypad;
+export default Keypad; // Export the component as the default export
